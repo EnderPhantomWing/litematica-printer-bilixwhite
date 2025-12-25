44675b6520d558d399517a9050973fc488c5c118
bunnyi116 <1160706050@qq.com>
2025-12-25 20:36:32 +0800

    移除无用 import

--- src/main/java/me/aleksilassila/litematica/printer/mixin/malilib/MixinGuiStringListEdit.java
+++ src/main/java/me/aleksilassila/litematica/printer/mixin/malilib/MixinGuiStringListEdit.java
@@ -12,7 +12,6 @@ import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiStringListEdit.class)
