078570da1520f859e7e22d98a7083171b8b5204a
bunnyi116 <1160706050@qq.com>
2025-12-26 12:04:36 +0800

    增强MessageUtils方法

--- src/main/java/me/aleksilassila/litematica/printer/utils/MessageUtils.java
+++ src/main/java/me/aleksilassila/litematica/printer/utils/MessageUtils.java
@@ -7,11 +7,25 @@ public class MessageUtils {
 
     public static final Minecraft client = Minecraft.getInstance();
 
-    public static void setOverlayMessage(Component message) {
-        client.gui.setOverlayMessage(message, false);
+    public static void setOverlayMessage(Component message, boolean bl) {
+        client.gui.setOverlayMessage(message, bl);
     }
 
     public static void addMessage(Component message) {
         client.gui.getChat().addMessage(message);
+    }
+
+    public static void setOverlayMessage(Component message) {
+        client.gui.setOverlayMessage(message, false);
+    }
+
+    // 扩展方法，普通字符串形式, 但并不建议使用, 因为没有做I18n
+    public static void setOverlayMessage(String message) {
+        setOverlayMessage(StringUtils.literal(message));
+    }
+
+    // 扩展方法，普通字符串形式, 但并不建议使用, 因为没有做I18n
+    public static void addMessage(String message) {
+        addMessage(StringUtils.literal(message));
     }
 }
