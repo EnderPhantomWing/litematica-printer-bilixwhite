f237f34d4d7c831c122c9cfc5a89b3fdd6eef7f0
bunnyi116 <1160706050@qq.com>
2026-01-05 23:20:01 +0800

    增强masa全家桶搜索（支持拼音）

--- src/main/java/me/aleksilassila/litematica/printer/mixin/printer/mc/MixinClientPacketListener.java
+++ src/main/java/me/aleksilassila/litematica/printer/mixin/printer/mc/MixinClientPacketListener.java
@@ -23,7 +23,7 @@ public abstract class MixinClientPacketListener {
             return;
         }
         if (packet.getHealth() == 0 && Configs.General.AUTO_DISABLE_PRINTER.getBooleanValue()) {
-            MessageUtils.setOverlayMessage(I18n.AUTO_DISABLE_NOTICE.getComponent().getString());
+            MessageUtils.setOverlayMessage(I18n.AUTO_DISABLE_NOTICE.getComponent());
             Configs.General.PRINT_SWITCH.setBooleanValue(false);
         }
     }
