03dfec36d8d33dc359007d781d4402e8cdb662aa
bunnyi116 <1160706050@qq.com>
2026-02-07 11:26:30 +0800

    修复mixin错误

--- src/main/java/me/aleksilassila/litematica/printer/mixin/openinv/InventoryMixin.java
+++ src/main/java/me/aleksilassila/litematica/printer/mixin/openinv/InventoryMixin.java
@@ -15,20 +15,11 @@ public interface InventoryMixin {
 
     @Inject(
             at = @At("HEAD"),
-            //#if MC >= 12004
-            method = "stillValidBlockEntity(Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/player/Player;F)Z",
-            //#else
-            //$$ method = "stillValidBlockEntity(Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/player/Player;I)Z",
-            //#endif
+            method = "stillValidBlockEntity(Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/player/Player;)Z",
             cancellable = true
     )
     private static void canPlayeruse(BlockEntity blockEntity,
                                      Player player,
-                                     //#if MC >= 12004
-                                     float range,
-                                     //#else
-                                     //$$ int range,
-                                     //#endif
                                      CallbackInfoReturnable<Boolean> cir) {
         if (player instanceof ServerPlayer) {
             for (ServerPlayer serverPlayer : OpenInventoryPacket.playerList) {
