f03d11da2f33246ebe3124493f6aab1586efc89d
bunnyi116 <1160706050@qq.com>
2026-02-10 00:10:32 +0800

    Revert "添加鼠标释放刷新配置界面"
    This reverts commit d4a945b67eebe07d3e2ef1e0eb385d2919b0df10.

--- src/main/java/me/aleksilassila/litematica/printer/gui/ConfigUi.java
+++ src/main/java/me/aleksilassila/litematica/printer/gui/ConfigUi.java
@@ -13,7 +13,6 @@ import me.aleksilassila.litematica.printer.config.Configs;
 import me.aleksilassila.litematica.printer.utils.UpdateCheckerUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.input.MouseButtonEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -46,12 +45,6 @@ public class ConfigUi extends GuiConfigsBase {
         reCreateListWidget();
         Objects.requireNonNull(getListWidget()).resetScrollbarPosition();
         initGui();
-    }
-
-    @Override
-    public boolean onMouseReleased(MouseButtonEvent click) {
-        this.reset();
-        return super.onMouseReleased(click);
     }
 
     private int createButton(int x, int y, int width, Tab tab) {
