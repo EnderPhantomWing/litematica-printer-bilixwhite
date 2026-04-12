c8e94c4c1d6d989b0b2bfdecfb8dc62f80d9b4cc
BlinkWhite <bilixwhite@qq.com>
2026-04-12 18:39:36 +0800

    feat: 添加农作物催熟功能(#97)

--- src/main/java/me/aleksilassila/litematica/printer/config/Configs.java
+++ src/main/java/me/aleksilassila/litematica/printer/config/Configs.java
@@ -451,6 +451,11 @@ public class Configs extends ConfigBuilders implements IConfigHandler {
                 .setVisible(FILL_COMPOSTER::getBooleanValue)
                 .build();
 
+        // 农作物催熟
+        public static final ConfigBoolean BONEMEAL_CROPS = bool("printBonemealCrops")
+                .defaultValue(false)
+                .build();
+
         // 下落方块检查
         public static final ConfigBoolean FALLING_CHECK = bool("printFallingBlockCheck")
                 .defaultValue(true)
@@ -491,7 +496,8 @@ public class Configs extends ConfigBuilders implements IConfigHandler {
                 NOTE_BLOCK_TUNING,
                 REPLACE_CORAL,
                 FILL_COMPOSTER,
-                FILL_COMPOSTER_WHITELIST
+                FILL_COMPOSTER_WHITELIST,
+                BONEMEAL_CROPS
         );
     }
 
