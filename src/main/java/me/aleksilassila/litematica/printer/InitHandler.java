c83a4afcce5954cea980f837e047807b9a82c8ce
EnderPhantomWing <EnderPhantomWing@outlook.com>
2026-06-19 22:02:32 +0800

    perf: Add a high-brightness renderer and optimize performance

--- src/main/java/me/aleksilassila/litematica/printer/InitHandler.java
+++ src/main/java/me/aleksilassila/litematica/printer/InitHandler.java
@@ -1,10 +1,12 @@
 package me.aleksilassila.litematica.printer;
 
+import fi.dy.masa.malilib.event.RenderEventHandler;
 import fi.dy.masa.malilib.interfaces.IInitializationHandler;
 import me.aleksilassila.litematica.printer.config.Configs;
 import me.aleksilassila.litematica.printer.enums.PrintModeType;
 import me.aleksilassila.litematica.printer.gui.ConfigUi;
 import me.aleksilassila.litematica.printer.printer.ActionManager;
+import me.aleksilassila.litematica.printer.render.BlockHighlightRenderer;
 import me.aleksilassila.litematica.printer.utils.MessageUtils;
 import me.aleksilassila.litematica.printer.render.MissingMaterialHudRenderer;
 import me.aleksilassila.litematica.printer.utils.ModUtils;
@@ -17,6 +19,8 @@ public class InitHandler implements IInitializationHandler {
         initConfigCallback();
         fi.dy.masa.litematica.render.infohud.InfoHud.getInstance()
                 .addInfoHudRenderer(MissingMaterialHudRenderer.INSTANCE, true);
+
+        RenderEventHandler.getInstance().registerWorldLastRenderer(new BlockHighlightRenderer());
     }
 
     private void initConfigCallback() {
